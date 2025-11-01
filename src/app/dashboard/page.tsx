05764d0924ae1e552d91acbc87460df9acb91305
NaKern <bajungjang@gmail.com>
2025-11-01 13:53:05 +0000

    nothing changed look at it

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -73,7 +73,7 @@ export default function DashboardPage() {
   return (
     <div className="flex flex-col gap-4">
       <SummaryCards />
-      <div className="grid gap-4 md:gap-8 grid-cols-1 lg:grid-cols-3">
+      <div className="grid gap-4 md:gap-8 lg:grid-cols-3">
         <div className="lg:col-span-2 flex flex-col gap-4">
             <Card>
               <CardHeader>
